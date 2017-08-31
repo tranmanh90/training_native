0257ec5864098ac2dd3cd4e05aa98cefed08970f
tranmanh90 <tranmanh90@users.noreply.github.com>
2017-09-01 00:33:27 +0700

    Added ViewServer to use hierarchy viewer

--- 003_LinearLayout/app/src/main/java/com/training/linearlayout/MainActivity.java
+++ 003_LinearLayout/app/src/main/java/com/training/linearlayout/MainActivity.java
@@ -1,13 +1,28 @@
 package com.training.linearlayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.debug.hv.ViewServer;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewServer.get(this).addWindow(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ViewServer.get(this).setFocusedWindow(this);
     }
 }
