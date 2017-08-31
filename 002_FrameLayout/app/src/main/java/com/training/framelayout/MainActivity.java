73a6be181901760d08d729babe6a57d0c24f5f9b
tranmanh90 <tranmanh90@users.noreply.github.com>
2017-08-31 17:07:22 +0700

    Add logic code for button1, button2

--- 002_FrameLayout/app/src/main/java/com/training/framelayout/MainActivity.java
+++ 002_FrameLayout/app/src/main/java/com/training/framelayout/MainActivity.java
@@ -2,12 +2,37 @@ package com.training.framelayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton mImageButton1;
+    private ImageButton mImageButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageButton1 = (ImageButton) findViewById(R.id.image_button_1);
+        mImageButton2 = (ImageButton) findViewById(R.id.image_button_2);
+        mImageButton1.setOnClickListener(this);
+        mImageButton2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.image_button_1:
+                mImageButton1.setVisibility(v.GONE);
+                mImageButton2.setVisibility(v.VISIBLE);
+                break;
+
+            case R.id.image_button_2:
+                mImageButton1.setVisibility(v.VISIBLE);
+                mImageButton2.setVisibility(v.GONE);
+                break;
+        }
     }
 }
