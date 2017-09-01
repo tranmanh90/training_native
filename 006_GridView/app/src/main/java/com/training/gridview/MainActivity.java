c412c65f9783cd11b49be18735df151497bd0059
tranmanh90 <tranmanh90@users.noreply.github.com>
2017-09-01 15:35:37 +0700

    Added logic code to get data from adapter

--- 006_GridView/app/src/main/java/com/training/gridview/MainActivity.java
+++ 006_GridView/app/src/main/java/com/training/gridview/MainActivity.java
@@ -1,7 +1,11 @@
 package com.training.gridview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
